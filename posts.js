7603d7f28ba9be6d3b21210a9cd8f1525b6c8e2a
ayush536 <82461982+ayush536@users.noreply.github.com>
2022-04-20 23:35:33 +0530

    final

---
+++ posts.js
@@ -0,0 +1,25 @@
+ const posts=[
+    {
+
+        "userId":1,
+        "id":1,
+        "title":"restaurent day out",
+        "body":"CCD"
+    },
+    {
+
+        "userId":1,
+        "id":2,
+        "title":"blog Day",
+        "body":"starbucks"
+    },
+    {
+
+        "userId":1,
+        "id":3,
+        "title":"MOVIE night",
+        "body":"KGF chapter-2"
+    }
+]
+
+module.exports=posts
